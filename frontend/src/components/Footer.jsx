55a2df75fd9eb55f916f16c53739d14871d5a7f1
mrrcodex <mrramadhandev@gmail.com>
2026-04-08 03:31:42 +0700

    feat: add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer className="bg-[#f8fafc] p-6">
+        <ul className="flex gap-30 justify-center">
+          <li>© 2024 SHORTLINK. THE DIGITAL ARCHITECT</li>
+          <li>PRIVACY AND POLICY</li>
+          <li>TERMS OF SERVICE</li>
+          <li>API DOCUMENTATION</li>
+          <li>SUPPORT</li>
+        </ul>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
