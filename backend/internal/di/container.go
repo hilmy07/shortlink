8827c8c6466570c590aecf2adc26ab6f51589b9d
mrrcodex <mrramadhandev@gmail.com>
2026-04-08 04:17:14 +0700

    fix: add conection to db and init container

--- backend/internal/di/container.go
+++ backend/internal/di/container.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hilmy07/shotlink/backend/internal/handlers"
+	"github.com/hilmy07/shotlink/backend/internal/repository"
+	"github.com/hilmy07/shotlink/backend/internal/service"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Container struct {
 	db *pgxpool.Pool
+
+	userRepo    *repository.UserRepository
+	linkRepo *repository.LinkRepository
+
+	authService *service.AuthService
+	authHandler *handlers.AuthHandler
+	linkService *service.LinkService
+	linkHandler *handlers.LinkHandler
 }
 
 func NewContainer(db *pgxpool.Pool) *Container {
@@ -24,7 +35,22 @@ func NewContainer(db *pgxpool.Pool) *Container {
 }
 
 func (c *Container) init() { 
+	c.userRepo = repository.NewUserRepository(c.db)
+	c.linkRepo = repository.NewLinkRepository(c.db)
 
+	c.authService = service.NewAuthService(c.userRepo)
+	c.authHandler = handlers.NewAuthHandler(c.authService)
+
+	c.linkService = service.NewLinkService(c.linkRepo)
+	c.linkHandler = handlers.NewLinkHandler(c.linkService)
+}
+
+func (c *Container) AuthHandler() *handlers.AuthHandler {
+	return c.authHandler
+}
+
+func (c *Container) LinkHandler() *handlers.LinkHandler {
+	return c.linkHandler
 }
 
 func Connect() (*pgxpool.Pool, error) {
